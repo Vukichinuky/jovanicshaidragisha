25ed388455474adeed09d7429951d5eba21134f3
Drago Vukovic <gudraliin@gmail.com>
2023-12-05 18:49:43 +0100

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Jovana i Drago',
   description: 'Jovana i Drago',
-  openGraph:{
-  openGraphImage: '/IMG_6122.JPG',}
+  
 }
 
 export default function RootLayout({
