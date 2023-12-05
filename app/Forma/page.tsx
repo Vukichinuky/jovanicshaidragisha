b08a65ba559a1b9f2b66ea955622c43a3b8d7411
Drago Vukovic <gudraliin@gmail.com>
2023-12-05 18:14:20 +0100

    asd

---
+++ app/Forma/page.tsx
@@ -0,0 +1,51 @@
+'use client'
+import React, { useState } from 'react'
+import Link from 'next/link'
+import EmailForm from '../../components/EmailForm'
+function Forma() {
+  const [answer1, setAnswer1] = useState('');
+  const [answer2, setAnswer2] = useState('');
+
+  const correctAnswer1 = 'pica'; // replace with your correct answer
+  const correctAnswer2 = '837497'; // replace with your correct answer
+
+  const isCorrect = answer1.toLowerCase() === correctAnswer1 && answer2.toLowerCase() === correctAnswer2;  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    setSubmitted(true);
+  };
+  return (
+    <div className='flex-col mt-56 gap-28 flex  items-center justify-center'>
+      {!submitted && (
+        <>
+          <div className='flex flex-col items-center gap-7 justify-center'>
+  <label className='text-sm font-semibold'>
+    Koja je omiljena hrana dragi na celom svetu?
+  </label>
+  <input type="text" className='border p-2 rounded-md ml-4' value={answer1} onChange={e => setAnswer1(e.target.value)} />
+</div>
+<div className='flex flex-col gap-7 items-center justify-center'>
+  <label className='text-sm font-semibold'>
+    Koji je dragi pin na telefonu
+  </label>
+  <input type="text" className='border p-2 rounded-md ml-4' value={answer2} onChange={e => setAnswer2(e.target.value)} />
+</div>
+          <button className=' rounded-lg border border-transparent border-gray-300 px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30' onClick={handleSubmit}>Nastavi</button>
+        </>
+      )}
+      {submitted && (
+        isCorrect ? (
+          <div>
+<EmailForm />
+
+          </div>
+        ) : (
+          <div className='flex items-center justify-center gap-7 felx-row'><p>Odgovori nisu tacni, pokusaj ponovo</p>       <button className=' rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30' onClick={() => window.location.reload()}>Nazad</button>
+          </div>
+        )
+      )}
+    </div>
+  )
+}
+
+export default Forma
---
+++ components/EmailForm.tsx
@@ -0,0 +1,81 @@
+'use client'
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+import Image from 'next/image'
+function EmailForm() {
+    const [form, setForm] = useState({  question1: '', question2: '', question3: '',question4: '', message:'' }); 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+      setForm({ ...form, [e.target.name]: e.target.value });
+    };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  
+    emailjs.send('service_ivhi3fh', 'template_hoohh4c', form, 'hQ5FLvng7576T2WDd')
+      .then((result) => {
+          console.log(result.text);
+          if (form.question3 === 'yes') {
+            window.alert('06.12.2023 u 6 da odemo na dejt budi spremna');
+          }
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form className='flex flex-col p-5 justify-center items-center gap-10' onSubmit={handleSubmit}>
+    <label className='border p-2 rounded-md'>
+Da li zelis da razgovaramo i pokusamo da resimo ovo?  <select name="question3" onChange={handleChange}>
+    <option value="">Izaberi...</option>
+    <option value="yes">Da</option>
+    <option value="no">Ne</option>
+  </select>
+</label>
+{form.question3 === 'yes' && <label className='border p-2 rounded-md'>
+  Hajde da poradimo na nama malo?
+  <select name="question4" onChange={handleChange}>
+    <option value="">Izaberi...</option>
+    <option value="yes">Hajde</option>
+    <option value="yes2">Hajde</option>
+  </select>
+</label> }
+
+    <label className='border p-2 rounded-md'>
+  Da li je jovana pozelela dragu?
+  <select className='p-3' name="question1" onChange={handleChange}>
+    <option value="">Izaberi...</option>
+    <option value="yes">Jeste</option>
+    <option value="no">Nije</option>
+  </select>
+</label>
+{form.question1 === 'yes' && <h1>I drago je pozeleo Jovanu</h1>}
+<label className='border p-2 rounded-md'>
+Koliko je Drago pozeljen ako jeste? btw na 10 dobijas sliku mace
+  <select className='p-3' name="question2" onChange={handleChange}>
+    <option value="">Izaberi...</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+
+  </select>
+</label>
+{form.question2 === '10' && <Image height={300} alt='slika mace'  width={300} src='/OIP.jpeg'/>}
+
+<label className='border p-2 flex items-center justify-center rounded-md'>
+  Imas li ti sta da kazes?:
+  <textarea className='border p-2 rounded-md' name="message" onChange={handleChange} />
+</label>
+
+   
+    <input className=' rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-400 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 cursor-pointer hover:dark:bg-neutral-800/30' type="submit" value="Posalji" />
+  </form>
+  );
+}
+
+export default EmailForm;
