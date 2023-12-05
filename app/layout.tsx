a2766a3b84275de49121419a5d0ac9a321cd2edc
Drago Vukovic <gudraliin@gmail.com>
2023-12-05 19:00:52 +0100

    asd

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,20 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Jovana i Drago',
   description: 'Jovana i Drago',
-  og:{
-  image: {
-    url: '/IMG_6122.JPG',
-    width: '1200',
-    height: '630',
-    alt: 'This is logo of 634Properties'
-  },}
+  og: {
+    title: 'Jovana i Drago',
+    description: 'Jovana i drago',
+    type: 'website',
+    url: 'http://www.jovanaidrago.com/',
+    image: {
+      url: '/IMG_6122.JPG',
+      width: '1200',
+      height: '630',
+      alt: 'This is jovaan i drago'
+    },
+    locale: 'en_US',
+    site_name: 'jovaanaidrago'
+  },
 }
 
 export default function RootLayout({
